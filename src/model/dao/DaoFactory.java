53ff271bedd807a37c26def7721ebecf494296e0
Isabel Henrique <6isinha6@gmail.com>
2024-01-23 13:55:02 -0300

    implementação da operação findById

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 //operações estáticas para instanciar o DAO
@@ -7,6 +8,6 @@ public class DaoFactory {
 
     //permite a instanciação do sellerdao
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection()); //conexão como argumento
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,13 +1,27 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 //implementação do DAO
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    //conexao com o banco de dados
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Department obj) {
 
@@ -25,7 +39,41 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                    "FROM seller INNER JOIN department " +
+                    "ON seller.DepartmentId = department.Id " +
+                    "WHERE seller.Id = ? ");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            //transformando a tabela para associação de objetos (seller - department)
+            if (rs.next()) {
+                Department dep = new Department(); //instanciando um departamento
+                dep.setId(rs.getInt("DepartmentId")); //valor do dept
+                dep.setName(rs.getString("DepName")); //valor do dept
+
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep); //Associação de objetos: objeto departamento
+                return obj;
+            }
+            return null; //id nao existe
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
