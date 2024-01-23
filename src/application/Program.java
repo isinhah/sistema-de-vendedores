67f0907d58eccdafdf9ce89e568114b9282f33ce
Isabel Henrique <6isinha6@gmail.com>
2024-01-23 17:08:31 -0300

    implementação da operação FindAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,21 +5,27 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao(); //instanciacao do sellerdao sem expor a implementação
 
-        System.out.println("TEST 1: SELLER FindById");
+        System.out.println("=== TEST 1: SELLER FindById ===");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
-        System.out.println("TEST 2: SELLER FindByDepartment");
+        System.out.println("=== TEST 2: SELLER FindByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("=== TEST 3: SELLER FindAll ===");
+        list = sellerDao.findAll();
 
         for (Seller obj : list) {
             System.out.println(obj);
