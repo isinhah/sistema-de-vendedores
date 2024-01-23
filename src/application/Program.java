7e0df3428ecfa84ceca3e419410dcc8def7ede0e
Isabel Henrique <6isinha6@gmail.com>
2024-01-23 13:01:44 -0300

    add entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department dept1 = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dept1);
+        System.out.println(dept1);
+        System.out.println(seller);
     }
-
 }
