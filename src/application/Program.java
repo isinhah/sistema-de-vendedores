dc96d7c236e19df5b680d5bbd517b7094d8f9b19
Isabel Henrique <6isinha6@gmail.com>
2024-01-23 14:01:04 -0300

    reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao(); //instanciacao do sellerdao sem expor a implementação
+
+        System.out.println("TEST 1: SELLER FindById = 3");
         Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -52,17 +52,9 @@ public class SellerDaoJDBC implements SellerDao {
 
             //transformando a tabela para associação de objetos (seller - department)
             if (rs.next()) {
-                Department dep = new Department(); //instanciando um departamento
-                dep.setId(rs.getInt("DepartmentId")); //valor do dept
-                dep.setName(rs.getString("DepName")); //valor do dept
-
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep); //Associação de objetos: objeto departamento
+                //Funções para instanciar Department e Seller
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null; //id nao existe
@@ -74,6 +66,24 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep); //Associação de objetos: objeto departamento
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId")); //valor do dept
+        dep.setName(rs.getString("DepName")); //valor do dept
+        return dep;
     }
 
     @Override
