8bbe746c08c0d89b8591ae94abef5abeed6d7eae
Isabel Henrique <6isinha6@gmail.com>
2024-01-24 12:19:54 -0300

    implementação da operação delete seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao(); //instanciacao do sellerdao sem expor a implementação
 
         System.out.println("=== TEST 1: SELLER FindById ===");
@@ -42,6 +45,14 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Atualização concluida! ");
+
+        System.out.println("=== TEST 4: SELLER Delete ===");
+        System.out.println("Insira o id do vendedor que vai ser excluido: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Id excluido. ");
+
+        sc.close();
 
     }
 }
